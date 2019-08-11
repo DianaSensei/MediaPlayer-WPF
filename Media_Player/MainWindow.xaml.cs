49ab5c3a08d4d46c2c20c7f1494bbc749ad21aad
DianaSensei <43933486+DianaSensei@users.noreply.github.com>
2019-08-11 10:19:44 +0700

    new Layout

--- Media_Player/MainWindow.xaml.cs
+++ Media_Player/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -15,7 +17,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WMPLib;
 
 namespace Media_Player
@@ -23,136 +24,183 @@ namespace Media_Player
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
 
         public bool IsStopped = false;
         public bool IsPaused = false;
         public bool IsPlaying = false;
-        List<Media> _players = new List<Media>();
+        private double contentHeight;
+        private double contentWidth;
+        private string currentMedia;
+        double cur = 0.0;
+        int stt = 0;
+        static string[] videoExt =
+        {
+            ".FLV",".AVI",".WMV",".MP4",".MPG",".MPEG",".M4V"
+        };
+        static string[] audioExt =
+        {
+            ".MP3",".WAV",".WAVE",".WMA"
+        };
+
+        BindingList<Media> currentPlaylist = new BindingList<Media>();
+        BindingList<Playlist> playlists = new BindingList<Playlist>();
+        WindowsMediaPlayer Player = new WindowsMediaPlayer();
+        WindowsMediaPlayer ReadDurationPlayer = new WindowsMediaPlayer();
+        IWMPPlaylist iWMPPlaylist;
+
+        public string CurrentMedia
+        {
+            get => currentMedia; set
+            {
+                currentMedia = value;
+                RaiseChangeEvent();
+            }
+        }
+
+        public double ContentHeight
+        {
+            get => contentHeight; set
+            {
+                contentHeight = value;
+                RaiseChangeEvent();
+            }
+        }
+
+        public double ContentWidth
+        {
+            get => contentWidth; set
+            {
+                contentWidth = value;
+                RaiseChangeEvent();
+            }
+        }
+
+        void RaiseChangeEvent([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainWindow()
         {
             InitializeComponent();
-     
+            list.ItemsSource = currentPlaylist;
+            this.DataContext = this;
+            ContentHeight = mainwindow.Height - 100;
+            contentWidth = mainwindow.Width - 230;
+            Combobox_playlist.ItemsSource = playlists;
+            list.ItemsSource = currentPlaylist;
+            iWMPPlaylist = Player.playlistCollection.newPlaylist("currentPlaylist");
         }
 
-        public class Media : INotifyPropertyChanged
+        void CreateMediaElement(string path)
         {
-            private string _filename;
-            private string _filepath;
-
-            public string filename
-            {
-                get => _filename; set
-                {
-                    _filename = value;
-                    RaiseChangeEvent();
-                }
-            }
-
-            public string filepath
-            {
-                get => _filepath; set
-                {
-                    _filepath = value;
-                    RaiseChangeEvent();
-                }
-            }
-
-            void RaiseChangeEvent([CallerMemberName]string name = "")
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(filename));
-            }
-
-            public event PropertyChangedEventHandler PropertyChanged;
+            MediaElement mediaElement = new MediaElement();
+            Canvas_content.Children.Add(mediaElement);
+            mediaElement.Width = mainwindow.Width - 230;
+            mediaElement.Height = mainwindow.Height - 100;
+            mediaElement.Source = new Uri(path);
+        }
+        int getDuration(string path)
+        {
+            int duration = 0;
+            ReadDurationPlayer.URL = path;
+            ReadDurationPlayer.controls.play();
+            duration = (int)ReadDurationPlayer.currentMedia.duration;
+            ReadDurationPlayer.controls.stop();
+            return duration;
+        }
+        string getDurationString(int duration)
+        {
+            string durationStr;
+            TimeSpan result = TimeSpan.FromSeconds(duration);
+            if (duration >= 3600)
+                durationStr = result.ToString("hh':'mm':'ss");
+            else durationStr = result.ToString("mm':'ss");
+            return durationStr;
+        }
+        bool IsAudioFile(string path)
+        {
+            return -1 != Array.IndexOf(audioExt, Path.GetExtension(path).ToUpperInvariant());
+        }
+        bool IsVideoFile(string path)
+        {
+            return -1 != Array.IndexOf(videoExt, Path.GetExtension(path).ToUpperInvariant());
         }
 
         private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            OpenFileDialog open = new OpenFileDialog
             {
-                Filter = "(mp3, wav, mp4, mov, wmv, mpg, avi, 3gp, flv)| *.mp3; *.wav; *.mp4; *.3gp; *.avi; *.mov; *.flv; *.wmv; *.mpg | all files | *.* ",
+                Filter = "(mp3, wav, mp4, wmv, mpg, avi, flv)| *.mp3; *.wav; *.mp4; *.avi; *.flv; *.wmv; *.mpg | all files | *.* ",
                 Multiselect = true
             };
-            if (openFileDialog1.ShowDialog() == true)
+            if (open.ShowDialog() == true)
             {
-                string[] filesname = openFileDialog1.SafeFileNames;
-                string[] filespath = openFileDialog1.FileNames;
-                
-                for (int i = 0; i < filesname.Length; i++)
-                {
-                    string lvi = i + 1 + ". " + filesname[i];
-                    list.Items.Add(lvi);
+                string[] filenames = open.SafeFileNames;
+                string[] filepaths = open.FileNames;
 
-                }
-                for (int i = 0; i < filesname.Length; i++)
+                for (int i = 0; i < filenames.Length; i++)
                 {
-                    _players.Add(new Media { filename = filesname[i], filepath = filespath[i] });
+                    if (IsAudioFile(filenames[i]) || IsVideoFile(filenames[i]))
+                    {
+                        stt += 1;
+                        string title = stt + ". " + filenames[i];
+                        AddNewMedia(filepaths[i]);
+                        IWMPMedia3 media = Player.newMedia(filepaths[i]) as IWMPMedia3;
+                        iWMPPlaylist.appendItem(media);
+
+                    }
+
                 }
             }
-
-
         }
-        double cur = 0.0;
+        void AddNewMedia(string str)
+        {
+            currentPlaylist.Add(new Media()
+            {
+                File_Path = str,
+                Duration_length = getDuration(str)
+            });
+        }
         private void Play_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (IsPlaying == false)
-            {
-                foreach (var _player in _players)
-                {
 
-                    Player.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(Player_PlayStateChange);
-                    Player.MediaError += new WMPLib._WMPOCXEvents_MediaErrorEventHandler(Player_MediaError);
-                    Player.URL = _player.filepath;
-                }
-                IsPlaying = true;
-            }
-            else
+            if (!IsPlaying)
             {
-                if (IsStopped == false)
+                if (IsPaused)
                 {
                     Player.controls.currentPosition = cur;
-                    Player.controls.play();
-                    IsStopped = true;
                 }
                 else
                 {
-                    cur = Player.controls.currentPosition;
-                    Player.controls.pause();
-                    IsStopped = false;
-                    IsPlaying = false;
+                    Player.currentPlaylist = iWMPPlaylist;
                 }
-            }
-        }
-        private void Player_PlayStateChange(int NewState)
-        {
-            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped)
-            {
-                this.Close();
-            }
-        }
-
-        private void List_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            WMPLib.WindowsMediaPlayer Player;
-            foreach ( var _player in _players)
-            {
-                Player = new WMPLib.WindowsMediaPlayer();
-                Player.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(Player_PlayStateChange);
-                Player.MediaError += new WMPLib._WMPOCXEvents_MediaErrorEventHandler(Player_MediaError);
-                Player.URL = _player.filepath;
+                IsStopped = false;
+                IsPaused = false;
+                IsPlaying = true;
                 Player.controls.play();
+            }
+            else
+            {
+                cur = Player.controls.currentPosition;
+                Player.controls.pause();
+                IsStopped = false;
+                IsPlaying = false;
+                IsPaused = true;
             }
         }
 
         private void Next10_Button_Click(object sender, RoutedEventArgs e)
         {
-            Player.controls.currentPosition += 10;
+            Player.controls.next();
         }
 
         private void Prev10_Button_Click(object sender, RoutedEventArgs e)
         {
-            Player.controls.currentPosition -= 10;
+            Player.controls.previous();
         }
 
         private void Replay_Button_Click(object sender, RoutedEventArgs e)
@@ -160,13 +208,43 @@ namespace Media_Player
             Player.controls.currentPosition = 0;
         }
 
-
-        private void Player_MediaError(object pMediaObject)
+        private void ComboBox_DropDownOpened(object sender, EventArgs e)
         {
-            MessageBox.Show("Cannot play media file.");
-            this.Close();
+            string[] file = Directory.GetFiles("C:/Users/Systemcall/OneDrive/Máy tính","*.pl");
+            playlists.Clear();
+            foreach (var i in file)
+                playlists.Add(CreatePlaylist(i));
+        }
+        private Playlist CreatePlaylist(string i)
+        {
+            Playlist pl = new Playlist();
+            pl.m_Path = i;
+            return pl;
+        }
+        private void Btn_SavePlaylist(object sender, RoutedEventArgs e)
+        {
+
         }
 
-        WMPLib.WindowsMediaPlayer Player = new WMPLib.WindowsMediaPlayer();
+        private void Btn_NewPlaylist(object sender, RoutedEventArgs e)
+        {
+            playlists.Clear();
+        }
+
+        private void Combobox_playlist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Playlist_Detail(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mainwindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ContentHeight = mainwindow.Height - 100;
+            contentWidth = mainwindow.Width - 230;
+        }
     }
 }
---
+++ Media_Player/Media.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Media_Player
+{
+    public class Media
+    {
+        public string File_Path { get; set; }
+        public int Duration_length { get; set; }
+        public string Description()
+        {
+            return Path.GetFileNameWithoutExtension(File_Path);
+        }
+    }
+    public class Playlist
+    {
+        public string m_Path { get; set; }
+        override public string ToString()
+        {
+            return Path.GetFileNameWithoutExtension(m_Path);
+        }
+    }
+}
